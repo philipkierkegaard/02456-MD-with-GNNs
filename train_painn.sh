ac9c5696fd939c26e1ee74b635f8f2695c599a27
philipkierkegaard <philipkierkegaard@hotmail.com>
2025-11-15 23:36:14 +0100

    hajkh

--- train_painn.sh
+++ train_painn.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 #BSUB -q gpuv100
 #BSUB -J painn_md17
-#BSUB -n 1
+#BSUB -n 4
 #BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=8GB]"
-#BSUB -M 8GB
+#BSUB -M 9GB
 #BSUB -gpu "num=1"
 #BSUB -W 24:00
 #BSUB -o logs/%J.out
 #BSUB -e logs/%J.err
 
+# Load CUDA
 module load cuda/12.1
-source /work3/s234873/02456-MD-with-GNNs/painn_env/bin/activate
 
-python3 src/train.py
+# Path to environment
+ENV=/work3/s234873/02456-MD-with-GNNs/painn_env
+
+# Activate
+source $ENV/bin/activate
+
+# Run script using venv python
+$ENV/bin/python src/train.py
